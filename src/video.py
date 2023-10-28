811f26e8facd63d3e6f53534bc75470227c66864
PolinaWednesday <kekssmail@gmail.com>
2023-10-28 13:12:08 +0300

    new6_YouTube comm

--- src/video.py
+++ src/video.py
@@ -4,22 +4,32 @@ import os
 
 class Video:
     """Класс для видео"""
+
     def __init__(self, video_id):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        api_key = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        video = video_response['items'][0]
-        self.title = video['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = int(video['statistics']['viewCount'])
-        self.like_count = int(video['statistics']['likeCount'])
+        try:
+            api_key = os.getenv('API_KEY')
+            youtube = build('youtube', 'v3', developerKey=api_key)
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            video = video_response['items'][0]
+            self.title = video['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = int(video['statistics']['viewCount'])
+            self.like_count = int(video['statistics']['likeCount'])
+
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+
     def __str__(self):
         """Выводит в консоль информацию о видео."""
         return f"{self.title}"
+
 
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
@@ -30,4 +40,3 @@ class PLVideo(Video):
     def __str__(self):
         """Выводит в консоль информацию о плейлисте."""
         return f"{self.title}"
-
